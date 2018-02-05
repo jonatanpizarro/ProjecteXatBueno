73b14439880dbed26f0fb28362a942ee6e0d117d
GenisTorresDonaire <gtorresdonaire@iesesteveterradas.cat>
2018-02-05 18:02:09 +0100

    laravel creado

---
+++ app/Http/Controllers/DenunciaController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Denuncia;
+use Illuminate\Http\Request;
+
+class DenunciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Denuncia  $denuncia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Denuncia $denuncia)
+    {
+        //
+    }
+
+}
