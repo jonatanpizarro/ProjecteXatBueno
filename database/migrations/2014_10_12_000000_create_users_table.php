1e767c2ba743967131595f67746486cb8b8d0b4f
GenisTorresDonaire <gtorresdonaire@iesesteveterradas.cat>
2018-02-06 19:34:56 +0100

    home, nickname

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name')->nullable(false);
+            $table->string('nickname')->unique()->nullable(false);
+            $table->string('email')->unique()->nullable(false);
+            $table->string('password')->nullable(false);
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">Datos Usuario</div>
+
+                <div class="panel-body">
+                    <p>Usuario: Cliente</p>
+                    <p>Nombre: Genis</p>
+                    <p>Email: gtorres@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
             <div class="panel panel-default">
                 <div class="panel-heading">Bienvenido!!</div>
 
@@ -15,6 +26,15 @@
                     @endif
 
                     Te has logeado correctamente!
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">Banner</div>
+
+                <div class="panel-body">
+                    
                 </div>
             </div>
         </div>
